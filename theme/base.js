864819007fa47e95588e92200b1aa76b613043e8
Bastian <nbdehlen@gmail.com>
2021-03-01 04:46:22 +0100

    Make base components

--- theme/base.js
+++ theme/base.js
@@ -1,5 +1,8 @@
+import React from "react"
 import styled from "styled-components"
-import colors from "./colors.json"
+import ccolors from "./colors.json"
+
+export const colors = ccolors
 
 export const Row = styled.View`
   display: flex;
@@ -7,17 +10,36 @@ export const Row = styled.View`
   justify-content: space-between;
 `
 export const Card = styled.View`
-  background-color: ${colors.dark};
+  background-color: ${colors.primary.color};
   border-radius: 16px;
-  padding: 16px 16px 8px 16px;
-  margin: 24px 32px 0px 32px;
+  padding: 20px;
+  margin: 24px 32px 24px 32px;
 `
 export const Fail = styled.Text`
   font-weight: bold;
+  font-size: 18px;
   color: ${colors.fail};
 `
-
 export const Success = styled.Text`
   font-weight: bold;
+  font-size: 18px;
   color: ${colors.success};
 `
+export const BaseText = styled.Text`
+  font-size: 18px;
+  color: ${colors.primary.onColor};
+`
+export const Container = styled.View`
+  /* margin: 24px 32px 24px 32px; */
+  background-color: ${colors.dark};
+`
+export const ImageWrapper = styled.View`
+  justify-content: center;
+  height: 200px;
+  width: 100%;
+`
+const SpacerView = styled.View`
+  ${({ h }) => h && `height:${h}px;`}
+  ${({ w }) => w && `width:${w}px;`}
+`
+export const Spacer = ({ h, w }) => <SpacerView h={h} w={w} />
